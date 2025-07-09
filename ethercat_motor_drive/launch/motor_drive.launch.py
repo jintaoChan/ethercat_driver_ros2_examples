563d51a0ecde4156f7a315c1095d48e41607d35c
JintaoChan <1010046132@qq.com>
2025-07-09 16:43:13 +0800

    UPDATE: add rz_t1 io module

--- ethercat_motor_drive/launch/motor_drive.launch.py
+++ ethercat_motor_drive/launch/motor_drive.launch.py
@@ -80,7 +80,7 @@ def generate_launch_description():
     controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["velocity_controller", "--param-file", robot_controllers],
+        arguments=["rz_t1_gpio_controller", "--param-file", robot_controllers],
     )
 
     my_controller_spawner = Node(
